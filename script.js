2bfd4528401f9a8bbe154b5a54269643de877c78
Attckturtle <89983007+Attckturtle@users.noreply.github.com>
2022-03-21 17:40:49 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,89 +1,32 @@
 let gameOver = false;
-let hasFruitBeenEaten = false;
+let unitHeight = 5;
+let unitWidth = 5;
 
-let snakeHeadId = document.getElementById("snakeHead");
-let applesId = document.getElementById("apples");
+class snake {
+    constructor() {
+        this.x = 250;
+        this.y = 250;
+        this.velocityX = 0;
+        this.velocityY = 0;
+        this.height = unitHeight;
+        this.width = unitWidth;
+    }
 
-const boardWidth = 2000;
-const boardHeight = 2000;
-
-const unitWidth = 10;
-const unitHeight = 10;
-
-let snakeX;
-let snakeY;
-let currentDirection = 'U';
-
-let applesX;
-let applesY;
-
-let score = 0;
-
-let lastRenderedTime = 0;
-
-window.addEventListener('keydown', (event) => {
-    currentDirection = event.key;
-    currentDirection = currentDirection.toUpperCase();
-})
-
-function setUp() {
-    gameOver = false;
-    movingRight = true;
-    setInterval(() => {
-        moveSnake();
-        //generateApples();
-        detectAppleCollisions();
-        detectCollisions();
-        setScore();
-    }, 1000)
-}
-
-function startSnake() {
-
-}
-
-function moveSnake() {
-        switch (currentDirection) {
-            case "W":
-                console.log("up");
-                break;
-            case "A":
-                console.log("left");
-                break;
-            case "S":
-                console.log("down");
-                break;
-            case "D":
-                console.log("right");
-                break;
-        }
-        detectAppleCollisions();
-        detectCollisions();
-}
-
-function generateApples() {
-    if (hasFruitBeenEaten) {
-        applesX = Math.random(boardWidth);
-        applesY = Math.random(boardHeight);
-        Math.floor(applesX);
-        Math.floor(applesY);
-        applesId.style.top = applesX;
-        applesId.style.left = applesY;
-        hasFruitBeenEaten = false;
+    move() {
+        this.x += Math.random(-1, 1);
+        this.x += Math.random(-1, 1);
+        this.style.top = this.x;
+        this.style.left = this.y
     }
 }
 
-function detectCollisions() {
+let snake1 = new snake();
+console.log(ball1);
+
+function setup() {
+    createCanvas(500, 500);
 }
 
-function detectAppleCollisions() {
-    if (applesX == snakeX && applesY == snakeY && !hasFruitBeenEaten) {
-        score++
-        hasFruitBeenEaten = true;
-        setScore()
-    }
-}
-
-function setScore() {
-    document.getElementById("scoreBoard").innerHTML = "score";
+function draw() {
+    snake1.move();
 }
